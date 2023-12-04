e3eeabc66bccf31dc64b35237dad7370b25bcb68
ser9330026 <ser9330026@yandex.ru>
2023-12-04 18:20:00 +0500

    Добавьте файлы проекта.

---
+++ Задача №1. ДЗ Наследование/Задача №1. ДЗ Наследование.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+class Figure
+{
+protected:
+    int a = 0;
+public:
+    int showFigure() {
+        return a;
+    }
+};
+
+class Triangel : public Figure
+{
+public:
+    Triangel() {
+        a = 3;
+    }
+};
+
+class Rectangle : public Figure
+{
+public:
+    Rectangle()
+    {
+        a = 4;
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    Figure f;
+    Triangel t;
+    Rectangle r;
+    std::cout << "Количество сторон: " << std::endl;
+    std::cout << "Фигура: " << f.showFigure() << std::endl;
+    std::cout << "Треугольник: " << t.showFigure() << std::endl;
+    std::cout << "Прямоугольник: " << r.showFigure() << std::endl;
+
+    return 0;
+}
